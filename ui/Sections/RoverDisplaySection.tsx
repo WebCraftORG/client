7e9f5b3585c00ddef468eb910fb6e9876eb04228
NWelde <nathanweldegiorgis731@gmail.com>
2024-05-11 21:50:44 +0300

    stopped using the next js helpers

--- ui/Sections/RoverDisplaySection.tsx
+++ ui/Sections/RoverDisplaySection.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { SupabaseClient } from "@supabase/supabase-js";
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 interface Rover {
   id: string;
@@ -13,9 +13,8 @@ function RoverDisplaySection() {
   const [itemsCollected, setItemsCollected] = useState<number>(0);
   const collectionIntervalRef = useRef<NodeJS.Timeout | null>(null);
 
-  // Initialize the Supabase client using createBrowserSupabaseClient
-  // This function automatically uses NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY from the environment
-  const supabase: SupabaseClient = createBrowserSupabaseClient();
+
+  const supabase: SupabaseClient = useSupabaseClient();
 
   useEffect(() => {
     async function fetchRovers() {
